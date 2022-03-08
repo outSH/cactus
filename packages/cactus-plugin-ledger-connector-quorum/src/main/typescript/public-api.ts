1019f61cf8545eda99c1be92cdb48d78c60cda19
Michal Bajer <michal.bajer@fujitsu.com>
2022-03-08 22:15:14 +0100

    build: monitoring is working
    Signed-off-by: Michal Bajer <michal.bajer@fujitsu.com>

--- packages/cactus-plugin-ledger-connector-quorum/src/main/typescript/public-api.ts
+++ packages/cactus-plugin-ledger-connector-quorum/src/main/typescript/public-api.ts
@@ -12,6 +12,13 @@ export { PluginFactoryLedgerConnector } from "./plugin-factory-ledger-connector"
 import { IPluginFactoryOptions } from "@hyperledger/cactus-core-api";
 import { PluginFactoryLedgerConnector } from "./plugin-factory-ledger-connector";
 
+export {
+  QuorumApiClient,
+  QuorumApiClientOptions,
+} from "./api-client/quorum-api-client";
+
+export * from "./generated/openapi/typescript-axios/api";
+
 export async function createPluginFactory(
   pluginFactoryOptions: IPluginFactoryOptions,
 ): Promise<PluginFactoryLedgerConnector> {
