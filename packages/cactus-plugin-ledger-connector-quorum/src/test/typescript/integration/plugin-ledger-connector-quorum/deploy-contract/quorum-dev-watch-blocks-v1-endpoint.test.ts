a1472344b006cc631cc4398cd3e28b56f0989c9b
Michal Bajer <michal.bajer@fujitsu.com>
2022-03-10 12:57:07 +0100

    build: fix private transaction test
    Signed-off-by: Michal Bajer <michal.bajer@fujitsu.com>

--- packages/cactus-plugin-ledger-connector-quorum/src/test/typescript/integration/plugin-ledger-connector-quorum/deploy-contract/quorum-dev-watch-blocks-v1-endpoint.test.ts
+++ packages/cactus-plugin-ledger-connector-quorum/src/test/typescript/integration/plugin-ledger-connector-quorum/deploy-contract/quorum-dev-watch-blocks-v1-endpoint.test.ts
@@ -49,42 +49,57 @@ const log: Logger = LoggerProvider.getOrCreate({
 });
 log.info("Test started");
 
+const ledgerData = {
+  tessera: {
+    member2: {
+      publicKey: "BULeR8JyUWhiuuCMU/HLA0Q5pzkYT+cHII3ZKBey3Bo=",
+    },
+    member1: {
+      publicKey: "QfeDAys9MPDs2XHExtc84jKGHxZg/aj52DTh0vtA3Xc=",
+    },
+    member3: {
+      publicKey: "1iTZde/ndBHvzhcl7V68x44Vx7pl8nwx9LqnM/AfJUg=",
+    },
+  },
+  quorum: {
+    member2: {
+      name: "member2",
+      url: "http://127.0.0.1:20000",
+      wsUrl: "ws://127.0.0.1:20001",
+      privateUrl: "http://127.0.0.1:9081",
+      privateKey:
+        "b9a4bd1539c15bcc83fa9078fe89200b6e9e802ae992f13cd83c853f16e8bed4",
+      accountAddress: "f0e2db6c8dc6c681bb5d6ad121a107f300e9b2b5",
+    },
+    member1: {
+      name: "member1",
+      url: "http://127.0.0.1:20002",
+      wsUrl: "ws://127.0.0.1:20003",
+      privateUrl: "http://127.0.0.1:9082",
+      privateKey:
+        "f18166704e19b895c1e2698ebc82b4e007e6d2933f4b31be23662dd0ec602570",
+      accountAddress: "ca843569e3427144cead5e4d5999a3d0ccf92b8e",
+    },
+    member3: {
+      name: "member3",
+      url: "http://127.0.0.1:20004",
+      wsUrl: "ws://127.0.0.1:20005",
+      privateUrl: "http://127.0.0.1:9083",
+      privateKey:
+        "4107f0b6bf67a3bc679a15fe36f640415cf4da6a4820affaac89c8b280dfd1b3",
+      accountAddress: "0fbdc686b912d7722dc86510934589e0aaf3b55a",
+    },
+  },
+};
+
 describe("Quorum Monitoring", () => {
   let server: http.Server;
   let connector: PluginLedgerConnectorQuorum;
   let apiClient: QuorumApiClient;
-  // let sourceEthAccountPubKey: string;
-  // let sourceEthAccountPrivKey: { privateKey: string };
-  // let targetEthAccount: Account;
-  const sourceEthAccountPubKey = "0x627306090abaB3A6e1400e9345bC60c78a8BEf57";
-  const sourceEthAccountPrivKey = {
-    privateKey:
-      "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3",
-  };
-  const targetEthAccount = "0xfe3b557e8fb62b89f4916b721be55ceb828dbd73";
-  const targetTMKey = "QfeDAys9MPDs2XHExtc84jKGHxZg/aj52DTh0vtA3Xc";
-  const otherEthAccount = "0xf17f52151EbEF6C7334FAD080c5704D77216b732";
-  const otherTMKey = "1iTZde/ndBHvzhcl7V68x44Vx7pl8nwx9LqnM/AfJUg=";
 
-  const rpcApiHttpHost = "http://localhost:20000";
-  const rpcApiWsHost = "ws://localhost:20001";
-  const web3 = new Web3(rpcApiHttpHost);
+  const web3 = new Web3(ledgerData.quorum.member1.url);
 
   let privateContract: Contract;
-
-  async function logBalances() {
-    // GET ACCOUNT A BALANCE
-    const accountABalance = web3.utils.fromWei(
-      await web3.eth.getBalance(sourceEthAccountPubKey),
-    );
-    log.info("Source has balance of: " + accountABalance);
-
-    // GET ACCOUNT B BALANCE
-    const accountBBalance = web3.utils.fromWei(
-      await web3.eth.getBalance(targetEthAccount),
-    );
-    log.info("Target has balance of: " + accountBBalance);
-  }
 
   const abi = [
     {
@@ -119,30 +134,64 @@ describe("Quorum Monitoring", () => {
     "608060405234801561001057600080fd5b50610150806100206000396000f3fe608060405234801561001057600080fd5b50600436106100365760003560e01c80632e64cec11461003b5780636057361d14610059575b600080fd5b610043610075565b60405161005091906100d9565b60405180910390f35b610073600480360381019061006e919061009d565b61007e565b005b60008054905090565b8060008190555050565b60008135905061009781610103565b92915050565b6000602082840312156100b3576100b26100fe565b5b60006100c184828501610088565b91505092915050565b6100d3816100f4565b82525050565b60006020820190506100ee60008301846100ca565b92915050565b6000819050919050565b600080fd5b61010c816100f4565b811461011757600080fd5b5056fea2646970667358221220404e37f487a89a932dca5e77faaf6ca2de3b991f93d230604b1b8daaef64766264736f6c63430008070033";
 
   async function createPrivateContract() {
-    var contractInstance = new web3.eth.Contract(abi as any);
-    var deployData = contractInstance
-      .deploy({
-        data: bytecode,
-      })
-      .encodeABI();
+    try {
+      var contractInstance = new web3.eth.Contract(abi as any);
+      var deployData = contractInstance
+        .deploy({
+          data: bytecode,
+        })
+        .encodeABI();
 
-    var tx = {
-      data: deployData,
-      from: sourceEthAccountPubKey,
-      privateFor: [targetTMKey],
-      gasLimit: "0x24A22",
-    };
+      var tx = {
+        data: deployData,
+        from: ledgerData.quorum.member1.accountAddress,
+        privateFor: [ledgerData.tessera.member3.publicKey],
+        gasLimit: "0x24A22",
+      };
 
-    const signedTx = await web3.eth.accounts.signTransaction(
-      tx,
-      sourceEthAccountPrivKey.privateKey,
+      const signedTx = await web3.eth.accounts.signTransaction(
+        tx,
+        ledgerData.quorum.member1.privateKey,
+      );
+
+      const deployReceipt = await web3.eth.sendSignedTransaction(
+        signedTx.rawTransaction as string,
+      );
+
+      return new web3.eth.Contract(abi as any, deployReceipt.contractAddress);
+    } catch (err) {
+      log.error(err);
+      throw err;
+    }
+  }
+
+  async function createContract() {
+    const contractInstance = new web3.eth.Contract(abi as any);
+    const ci = await contractInstance
+      .deploy({ data: "0x" + bytecode })
+      .send({
+        from: ledgerData.quorum.member1.accountAddress,
+        privateFor: [ledgerData.tessera.member3.publicKey],
+        gasLimit: "0x24A22",
+      } as any)
+      .on("transactionHash", function (hash) {
+        console.log("The transaction hash is: " + hash);
+      });
+    return ci;
+  }
+
+  async function logBalances() {
+    // GET ACCOUNT A BALANCE
+    const accountABalance = web3.utils.fromWei(
+      await web3.eth.getBalance(ledgerData.quorum.member1.accountAddress),
     );
+    log.info("Source has balance of: " + accountABalance);
 
-    const deployReceipt = await web3.eth.sendSignedTransaction(
-      signedTx.rawTransaction as string,
+    // GET ACCOUNT B BALANCE
+    const accountBBalance = web3.utils.fromWei(
+      await web3.eth.getBalance(ledgerData.quorum.member2.accountAddress),
     );
-
-    return new web3.eth.Contract(abi as any, deployReceipt.contractAddress);
+    log.info("Target has balance of: " + accountBBalance);
   }
 
   //////////////////////////////////
@@ -166,20 +215,14 @@ describe("Quorum Monitoring", () => {
     // const rpcApiWsHost = await besuTestLedger.getRpcApiWsHost();
 
     // NOTE - RUN QUORUM MULTI PARTY MANUALLY
-    log.debug("rpcApiHttpHost:", rpcApiHttpHost);
-    log.debug("rpcApiWsHost:", rpcApiWsHost);
+    log.debug("rpcApiHttpHost:", ledgerData.quorum.member1.url);
+    log.debug("rpcApiWsHost:", ledgerData.quorum.member1.wsUrl);
 
     // Source account - genesis account
     // sourceEthAccountPubKey = besuTestLedger.getGenesisAccountPubKey();
     // sourceEthAccountPrivKey = {
     //   privateKey: besuTestLedger.getGenesisAccountPrivKey(),
     // };
-
-    const senderAccount = web3.eth.accounts.privateKeyToAccount(
-      sourceEthAccountPrivKey.privateKey,
-    );
-    log.debug(senderAccount.address);
-    log.debug(senderAccount.address == sourceEthAccountPubKey);
 
     log.info("Create PluginLedgerConnectorQuorum...");
     const keychainPlugin = new PluginKeychainMemory({
@@ -192,8 +235,8 @@ describe("Quorum Monitoring", () => {
     });
 
     connector = new PluginLedgerConnectorQuorum({
-      rpcApiHttpHost,
-      rpcApiWsHost,
+      rpcApiHttpHost: ledgerData.quorum.member1.url,
+      rpcApiWsHost: ledgerData.quorum.member1.wsUrl,
       logLevel: sutLogLevel,
       instanceId: uuidv4(),
       pluginRegistry: new PluginRegistry({ plugins: [keychainPlugin] }),
@@ -228,9 +271,14 @@ describe("Quorum Monitoring", () => {
     } as any);
 
     log.info(">>> DEPLOY PRIVATE CONTRACT");
-    privateContract = await createPrivateContract();
+    try {
+      //privateContract = await createPrivateContract();
+      privateContract = await createContract();
+    } catch (err) {
+      log.error(err);
+      throw err;
+    }
 
-    log.info(">>> BALANCES BEFORE");
     await logBalances();
   });
 
@@ -245,8 +293,6 @@ describe("Quorum Monitoring", () => {
     // await besuTestLedger.destroy();
     // log.info("Prune docker...");
     // await pruneDockerAllIfGithubAction({ logLevel: testLogLevel });
-
-    log.info(">>> BALANCES AFTER");
     await logBalances();
   });
 
@@ -255,12 +301,12 @@ describe("Quorum Monitoring", () => {
 
     const { rawTransaction } = await web3.eth.accounts.signTransaction(
       {
-        from: sourceEthAccountPubKey,
-        to: targetEthAccount,
-        value: 88888,
-        gas: 1000000,
+        from: ledgerData.quorum.member1.accountAddress,
+        to: ledgerData.quorum.member2.accountAddress,
+        value: 8,
+        gas: 100,
       },
-      sourceEthAccountPrivKey.privateKey,
+      ledgerData.quorum.member1.privateKey,
     );
 
     log.info("rawTransaction:", rawTransaction);
@@ -274,11 +320,6 @@ describe("Quorum Monitoring", () => {
       },
     } as any);
   }
-
-  // test("Sanity send transaction works", async () => {
-  //   await sendTransactionOnBesuLedger();
-  //   expect(1).toBe(1);
-  // });
 
   test("QuorumApiClient watchBlocksV1 works", async () => {
     const newBlock = new Promise<WatchBlocksV1Progress>((resolve, reject) => {
@@ -296,14 +337,14 @@ describe("Quorum Monitoring", () => {
         });
     });
 
-    await sendTransactionOnBesuLedger();
+    //await sendTransactionOnBesuLedger();
     return expect(newBlock).toResolve();
   });
 
   test("web3Eth test", async () => {
     const contract = {};
     const method = { type: "web3Eth", command: "getBalance" };
-    const args = { args: [sourceEthAccountPubKey] };
+    const args = { args: [ledgerData.quorum.member1.accountAddress] };
 
     apiClient.sendAsyncRequest(contract, method, args);
 
@@ -311,50 +352,105 @@ describe("Quorum Monitoring", () => {
     expect(results.data > 100);
   });
 
+  // async function getValueAtAddress(host:string, nodeName="node", deployedContractAbi: any, deployedContractAddress: any){
+  //   const web3 = new Web3(host);
+  //   const contractInstance = new web3.eth.Contract(deployedContractAbi, deployedContractAddress);
+  //   // contractInstance.defaultCommon.customChain = {name: 'GoQuorum', chainId: 1337};
+  //   const res = await contractInstance.methods.get().call().catch(() => {});
+  //   log.warn(nodeName + " obtained value at deployed contract is: "+ res);
+  //   return res
+  // }
+
+  // You need to use the accountAddress details provided to Quorum to send/interact with contracts
+  async function setValueAtAddress(
+    host: string,
+    value: number,
+    deployedContractAbi: any,
+    deployedContractAddress: any,
+    fromAddress: any,
+    toPublicKey: any,
+  ) {
+    const web3 = new Web3(host);
+    const contractInstance = new web3.eth.Contract(
+      deployedContractAbi,
+      deployedContractAddress,
+    );
+    const res = await contractInstance.methods
+      .store(value)
+      .send({
+        from: fromAddress,
+        privateFor: [toPublicKey],
+        gasLimit: "0x24A22",
+      });
+    // verify the updated value
+    // const readRes = await contractInstance.methods.get().call();
+    // console.log("Obtained value at deployed contract is: "+ readRes);
+    log.warn(res);
+    return res;
+  }
+
   test("web3EthContract manual test", async () => {
     const res = await privateContract.methods
       .retrieve()
-      .call({ from: sourceEthAccountPubKey });
+      .call({ from: ledgerData.quorum.member1.accountAddress });
     expect(res).toEqual("0");
+    log.error("first: " + res);
 
-    const reqAbi = await privateContract.methods.store(125).encodeABI();
+    // const reqAbi = await privateContract.methods.store(125).encodeABI();
 
-    var tx = {
-      data: reqAbi,
-      from: sourceEthAccountPubKey,
-      nonce: await web3.eth.getTransactionCount(sourceEthAccountPubKey),
-      to: privateContract.options.address,
-      privateFor: [targetTMKey],
-      gasLimit: "0x24A222",
-    };
+    // var tx = {
+    //   data: reqAbi,
+    //   from: ledgerData.quorum.member1.accountAddress,
+    //   // nonce: await web3.eth.getTransactionCount(
+    //   //   ledgerData.quorum.member1.accountAddress,
+    //   // ),
+    //   to: privateContract.options.address,
+    //   privateFor: [ledgerData.tessera.member3.publicKey],
+    //   gasLimit: "0x24A222",
+    // };
 
-    const signedTx = await web3.eth.accounts.signTransaction(
-      tx,
-      sourceEthAccountPrivKey.privateKey,
+    // log.debug(tx);
+
+    // const signedTx = await web3.eth.accounts.signTransaction(
+    //   tx,
+    //   ledgerData.quorum.member1.privateKey,
+    // );
+
+    // const sendRes = await web3.eth.sendSignedTransaction(
+    //   signedTx.rawTransaction as string,
+    // );
+    // log.warn(sendRes);
+    // log.warn(await web3.eth.getTransaction(sendRes.transactionHash));
+    await setValueAtAddress(
+      ledgerData.quorum.member1.url,
+      125,
+      abi,
+      privateContract.options.address,
+      ledgerData.quorum.member1.accountAddress,
+      ledgerData.tessera.member3.publicKey,
     );
-
-    const sendRes = await web3.eth.sendSignedTransaction(
-      signedTx.rawTransaction as string,
-    );
-    log.warn(sendRes);
-    log.warn(await web3.eth.getTransaction(sendRes.transactionHash));
 
     const getAfterRes = await privateContract.methods
       .retrieve()
-      .call({ from: targetEthAccount });
+      .call({ from: ledgerData.quorum.member1.accountAddress });
     expect(getAfterRes).toEqual("125");
+    log.error("getAfterRes: " + getAfterRes);
 
     // should be not visible??
-    const web33 = new Web3("http://localhost:20004");
-    const anotherContractInstance = new web33.eth.Contract(
-      abi as any,
-      privateContract.options.address,
-    );
-    const getAfterResOtherGuy = await anotherContractInstance.methods
-      .retrieve()
-      .call({ from: otherEthAccount });
-    log.warn(getAfterResOtherGuy);
-    expect(getAfterResOtherGuy).toEqual("125");
+    // const web33 = new Web3(ledgerData.quorum.member2.url);
+    // expect(async () => {
+    //   const anotherContractInstance = new web33.eth.Contract(
+    //     abi as any,
+    //     privateContract.options.address,
+    //   );
+    //   const getAfterResOtherGuy = await anotherContractInstance.methods
+    //     .retrieve()
+    //     .call({ from: ledgerData.quorum.member2.accountAddress });
+    //   log.warn(getAfterResOtherGuy);
+    // }).toThrow();
+
+    // expect(getAfterResOtherGuy).toEqual("125");
+    // log.error("getAfterResOtherGuy: " + getAfterResOtherGuy);
   });
 
   // test("web3EthContract test", async () => {
