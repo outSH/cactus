7b6322caca8d90322091efc73adb35b49d889060
Michal Bajer <michal.bajer@fujitsu.com>
2024-04-24 12:44:30 +0200

    feat(wip): add icons on  dashboard page

--- packages/cacti-ledger-browser/src/main/typescript/apps/eth/pages/Dashboard/Dashboard.tsx
+++ packages/cacti-ledger-browser/src/main/typescript/apps/eth/pages/Dashboard/Dashboard.tsx
@@ -1,9 +1,33 @@
-import TransactionSummary from "./TransactionSummary";
 import Box from "@mui/material/Box";
-import PageTitle from "../../../../components/ui/PageTitle";
 import Stack from "@mui/material/Stack";
-import { Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import { SvgIconComponent } from "@mui/icons-material";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import HubIcon from "@mui/icons-material/Hub";
+
+import PageTitle from "../../../../components/ui/PageTitle";
+import TransactionSummary from "./TransactionSummary";
 import BlockSummary from "./BlockSummary";
+
+interface TitleWithIconProps {
+  icon: SvgIconComponent;
+  children: React.ReactNode;
+}
+
+const TitleWithIcon: React.FC<TitleWithIconProps> = ({
+  children,
+  icon: Icon,
+}) => {
+  return (
+    <Box display="flex" alignItems="center" marginBottom={2}>
+      <Icon sx={{ fontSize: 35 }} />
+      <Typography variant="h6" component="h3" marginLeft={1}>
+        {children}
+      </Typography>
+    </Box>
+  );
+};
 
 function Dashboard() {
   return (
@@ -12,20 +36,17 @@ function Dashboard() {
       <Stack
         direction={{ lg: "column", xl: "row" }}
         spacing={5}
+        divider={<Divider orientation="vertical" flexItem />}
         justifyContent="space-between"
         alignItems="center"
       >
         <Box width={"80%"}>
-          <Typography variant="h6" component="h3" marginBottom={2}>
-            Blocks
-          </Typography>
+          <TitleWithIcon icon={HubIcon}>Blocks</TitleWithIcon>
           <BlockSummary />
         </Box>
 
         <Box width={"80%"}>
-          <Typography variant="h6" component="h3" marginBottom={2}>
-            Transactions
-          </Typography>
+          <TitleWithIcon icon={ReceiptOutlinedIcon}>Transactions</TitleWithIcon>
           <TransactionSummary />
         </Box>
       </Stack>
