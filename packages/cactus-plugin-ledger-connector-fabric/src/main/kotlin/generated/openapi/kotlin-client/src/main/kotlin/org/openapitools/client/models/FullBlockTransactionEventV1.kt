edsonCVN/cacti-edson-fork

feat(fabric-connector): add getChainInfo, improve getBlock output
- Add new method `getChainInfo` for quering chain information from qscc.
- Add `GetChainInfoEndpointV1` to allow calling `getChainInfo` remotely.
- Refactor `getBlock` so it can return same custom block formats
  as `WatchBlocks`. Default remains the same (full decode block).
  BREAKING CHANGE: It accepts `type` instead of `skipDecode` flag.
- Move common block formatting logic to `cacti-block-formatters.ts`.
- Add tests for new features. Move test common to quering `qscc` to single file
  to increase CI speed.

Signed-off-by: Michal Bajer <michal.bajer@fujitsu.com>

packages/cactus-plugin-ledger-connector-fabric/src/main/kotlin/generated/openapi/kotlin-client/src/main/kotlin/org/openapitools/client/models/FullBlockTransactionEventV1.kt
@@ -1,0 +1,60 @@
+/**
+ *
+ * Please note:
+ * This class is auto generated by OpenAPI Generator (https://openapi-generator.tech).
+ * Do not edit this file manually.
+ *
+ */
+
+@file:Suppress(
+    "ArrayInDataClass",
+    "EnumEntryName",
+    "RemoveRedundantQualifierName",
+    "UnusedImport"
+)
+
+package org.openapitools.client.models
+
+import org.openapitools.client.models.FullBlockTransactionActionV1
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+/**
+ * Transaction returned from fabric block.
+ *
+ * @param hash 
+ * @param channelId 
+ * @param timestamp 
+ * @param protocolVersion 
+ * @param transactionType 
+ * @param epoch 
+ * @param actions 
+ */
+
+
+data class FullBlockTransactionEventV1 (
+
+    @Json(name = "hash")
+    val hash: kotlin.String,
+
+    @Json(name = "channelId")
+    val channelId: kotlin.String,
+
+    @Json(name = "timestamp")
+    val timestamp: kotlin.String,
+
+    @Json(name = "protocolVersion")
+    val protocolVersion: java.math.BigDecimal,
+
+    @Json(name = "transactionType")
+    val transactionType: kotlin.String,
+
+    @Json(name = "epoch")
+    val epoch: java.math.BigDecimal,
+
+    @Json(name = "actions")
+    val actions: kotlin.collections.List<FullBlockTransactionActionV1>
+
+)
+

packages/cactus-plugin-ledger-connector-fabric/src/test/typescript/integration/fabric-v2-2-x/fabric-watch-blocks-delegated-sign-v1-endpoint.test.ts
@@ -386,22 +386,22 @@ describe("watchBlocksDelegatedSignV1 of fabric connector tests", () => {
   });
 
   /**
-   * Check Cactus custom transactions summary block monitoring.
+   * Check Cacti custom transactions summary block monitoring.
    */
-  test("Monitoring with type CactusTransactions returns transactions summary", async () => {
+  test("Monitoring with type CactiTransactions returns transactions summary", async () => {
     const monitorPromise = testWatchBlock(
-      "CactusTransactionsTest",
-      WatchBlocksListenerTypeV1.CactusTransactions,
+      "CactiTransactionsTest",
+      WatchBlocksListenerTypeV1.CactiTransactions,
       (event) => {
         expect(event).toBeTruthy();
 
-        if (!("cactusTransactionsEvents" in event)) {
+        if (!("cactiTransactionsEvents" in event)) {
           throw new Error(
             `Unexpected response from the connector: ${JSON.stringify(event)}`,
           );
         }
 
-        const eventData = event.cactusTransactionsEvents;
+        const eventData = event.cactiTransactionsEvents;
         expect(eventData.length).toBeGreaterThan(0);
         expect(eventData[0].chaincodeId).toBeTruthy();
         expect(eventData[0].transactionId).toBeTruthy();
@@ -413,9 +413,66 @@ describe("watchBlocksDelegatedSignV1 of fabric connector tests", () => {
     await monitorPromise;
   });
 
+  /**
+   * Check Cacti custom full block summary block monitoring.
+   */
+  test("Monitoring with type CactiFullBlock returns block summary", async () => {
+    const monitorPromise = testWatchBlock(
+      "CactiFullBlockTest",
+      WatchBlocksListenerTypeV1.CactiFullBlock,
+      (event) => {
+        expect(event).toBeTruthy();
+
+        if (!("cactiFullEvents" in event)) {
+          throw new Error(
+            `Unexpected response from the connector: ${JSON.stringify(event)}`,
+          );
+        }
+
+        const cactiFullBlock = event.cactiFullEvents;
+
+        // Check block fields
+        expect(cactiFullBlock).toBeTruthy();
+        expect(cactiFullBlock.blockNumber).toBeDefined();
+        expect(cactiFullBlock.blockHash).toBeTruthy();
+        expect(cactiFullBlock.previousBlockHash).toBeTruthy();
+        expect(cactiFullBlock.transactionCount).toBeDefined();
+
+        // Check transaction fields
+        for (const tx of cactiFullBlock.cactiTransactionsEvents) {
+          expect(tx.hash).toBeTruthy();
+          expect(tx.channelId).toBeTruthy();
+          expect(tx.timestamp).toBeTruthy();
+          expect(tx.transactionType).toBeTruthy();
+          expect(tx.protocolVersion).not.toBeUndefined();
+          expect(tx.epoch).not.toBeUndefined();
+
+          // Check transaction actions fields
+          for (const action of tx.actions) {
+            expect(action.functionName).toBeTruthy();
+            expect(action.functionArgs).toBeTruthy();
+            expect(action.functionArgs.length).toEqual(5);
+            expect(action.chaincodeId).toBeTruthy();
+            expect(action.creator.mspid).toBeTruthy();
+            expect(action.creator.cert).toBeTruthy();
+
+            // Check transaction action endorsement fields
+            for (const endorsement of action.endorsements) {
+              expect(endorsement.signature).toBeTruthy();
+              expect(endorsement.signer.mspid).toBeTruthy();
+              expect(endorsement.signer.cert).toBeTruthy();
+            }
+          }
+        }
+      },
+    );
+
+    await monitorPromise;
+  });
+
   test("Invalid WatchBlocksListenerTypeV1 value gets knocked down", async () => {
     const monitorPromise = testWatchBlock(
-      "CactusTransactionsTest",
+      "InvalidTypeTest",
       "Some_INVALID_WatchBlocksListenerTypeV1" as WatchBlocksListenerTypeV1,
       () => undefined, // will never reach this because it is meant to error out
       false,

packages/cactus-plugin-ledger-connector-fabric/src/test/typescript/integration/fabric-v2-2-x/fabric-watch-blocks-v1-endpoint.test.ts
@@ -60,8 +60,8 @@ const ledgerChannelName = "mychannel";
 const ledgerContractName = "basic";
 
 // Log settings
-const testLogLevel: LogLevelDesc = "TRACE"; // default: info
-const sutLogLevel: LogLevelDesc = "TRACE"; // default: info
+const testLogLevel: LogLevelDesc = "info"; // default: info
+const sutLogLevel: LogLevelDesc = "info"; // default: info
 
 // Logger setup
 const log: Logger = LoggerProvider.getOrCreate({
@@ -384,22 +384,22 @@ describe("watchBlocksV1 of fabric connector tests", () => {
   });
 
   /**
-   * Check Cactus custom transactions summary block monitoring.
+   * Check Cacti custom transactions summary block monitoring.
    */
-  test("Monitoring with type CactusTransactions returns transactions summary", async () => {
+  test("Monitoring with type CactiTransactions returns transactions summary", async () => {
     const monitorPromise = testWatchBlock(
-      "CactusTransactionsTest",
-      WatchBlocksListenerTypeV1.CactusTransactions,
+      "CactiTransactionsTest",
+      WatchBlocksListenerTypeV1.CactiTransactions,
       (event) => {
         expect(event).toBeTruthy();
 
-        if (!("cactusTransactionsEvents" in event)) {
+        if (!("cactiTransactionsEvents" in event)) {
           throw new Error(
             `Unexpected response from the connector: ${JSON.stringify(event)}`,
           );
         }
 
-        const eventData = event.cactusTransactionsEvents;
+        const eventData = event.cactiTransactionsEvents;
         expect(eventData.length).toBeGreaterThan(0);
         expect(eventData[0].chaincodeId).toBeTruthy();
         expect(eventData[0].transactionId).toBeTruthy();
@@ -411,9 +411,66 @@ describe("watchBlocksV1 of fabric connector tests", () => {
     await monitorPromise;
   });
 
+  /**
+   * Check Cacti custom full block summary block monitoring.
+   */
+  test("Monitoring with type CactiFullBlock returns block summary", async () => {
+    const monitorPromise = testWatchBlock(
+      "CactiFullBlockTest",
+      WatchBlocksListenerTypeV1.CactiFullBlock,
+      (event) => {
+        expect(event).toBeTruthy();
+
+        if (!("cactiFullEvents" in event)) {
+          throw new Error(
+            `Unexpected response from the connector: ${JSON.stringify(event)}`,
+          );
+        }
+
+        const cactiFullBlock = event.cactiFullEvents;
+
+        // Check block fields
+        expect(cactiFullBlock).toBeTruthy();
+        expect(cactiFullBlock.blockNumber).toBeDefined();
+        expect(cactiFullBlock.blockHash).toBeTruthy();
+        expect(cactiFullBlock.previousBlockHash).toBeTruthy();
+        expect(cactiFullBlock.transactionCount).toBeDefined();
+
+        // Check transaction fields
+        for (const tx of cactiFullBlock.cactiTransactionsEvents) {
+          expect(tx.hash).toBeTruthy();
+          expect(tx.channelId).toBeTruthy();
+          expect(tx.timestamp).toBeTruthy();
+          expect(tx.transactionType).toBeTruthy();
+          expect(tx.protocolVersion).not.toBeUndefined();
+          expect(tx.epoch).not.toBeUndefined();
+
+          // Check transaction actions fields
+          for (const action of tx.actions) {
+            expect(action.functionName).toBeTruthy();
+            expect(action.functionArgs).toBeTruthy();
+            expect(action.functionArgs.length).toEqual(5);
+            expect(action.chaincodeId).toBeTruthy();
+            expect(action.creator.mspid).toBeTruthy();
+            expect(action.creator.cert).toBeTruthy();
+
+            // Check transaction action endorsement fields
+            for (const endorsement of action.endorsements) {
+              expect(endorsement.signature).toBeTruthy();
+              expect(endorsement.signer.mspid).toBeTruthy();
+              expect(endorsement.signer.cert).toBeTruthy();
+            }
+          }
+        }
+      },
+    );
+
+    await monitorPromise;
+  });
+
   test("Invalid WatchBlocksListenerTypeV1 value gets knocked down", async () => {
     const monitorPromise = testWatchBlock(
-      "CactusTransactionsTest",
+      "CactiInvalidTest",
       "Some_INVALID_WatchBlocksListenerTypeV1" as WatchBlocksListenerTypeV1,
       () => undefined, // will never reach this because it is meant to error out
       false,

packages/cactus-plugin-persistence-fabric/src/main/typescript/plugin-persistence-fabric.ts
@@ -23,6 +23,7 @@ import {
   FabricApiClient,
   GetBlockResponseV1,
   RunTransactionResponseType,
+  GetBlockResponseTypeV1,
 } from "@hyperledger/cactus-plugin-ledger-connector-fabric";
 
 import PostgresDatabaseClient from "./db-client/db-client";
@@ -416,7 +417,7 @@ export class PluginPersistenceFabric
         query: {
           blockNumber,
         },
-        skipDecode: false,
+        responseType: GetBlockResponseTypeV1.Full,
       });
 
     const tempBlockParse = block.data;

outSH/cactus

packages/cactus-plugin-ledger-connector-fabric/src/main/kotlin/generated/openapi/kotlin-client/src/main/kotlin/org/openapitools/client/models/FullBlockTransactionEventV1.kt
@@ -1,0 +1,60 @@
+/**
+ *
+ * Please note:
+ * This class is auto generated by OpenAPI Generator (https://openapi-generator.tech).
+ * Do not edit this file manually.
+ *
+ */
+
+@file:Suppress(
+    "ArrayInDataClass",
+    "EnumEntryName",
+    "RemoveRedundantQualifierName",
+    "UnusedImport"
+)
+
+package org.openapitools.client.models
+
+import org.openapitools.client.models.FullBlockTransactionActionV1
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+/**
+ * Transaction returned from fabric block.
+ *
+ * @param hash 
+ * @param channelId 
+ * @param timestamp 
+ * @param protocolVersion 
+ * @param type 
+ * @param epoch 
+ * @param actions 
+ */
+
+
+data class FullBlockTransactionEventV1 (
+
+    @Json(name = "hash")
+    val hash: kotlin.String,
+
+    @Json(name = "channelId")
+    val channelId: kotlin.String,
+
+    @Json(name = "timestamp")
+    val timestamp: kotlin.String,
+
+    @Json(name = "protocolVersion")
+    val protocolVersion: java.math.BigDecimal,
+
+    @Json(name = "type")
+    val type: kotlin.String,
+
+    @Json(name = "epoch")
+    val epoch: java.math.BigDecimal,
+
+    @Json(name = "actions")
+    val actions: kotlin.collections.List<FullBlockTransactionActionV1>
+
+)
+

packages/cactus-plugin-ledger-connector-fabric/src/test/typescript/integration/fabric-v2-2-x/fabric-watch-blocks-delegated-sign-v1-endpoint.test.ts
@@ -386,22 +386,22 @@ describe("watchBlocksDelegatedSignV1 of fabric connector tests", () => {
   });
 
   /**
-   * Check Cactus custom transactions summary block monitoring.
+   * Check Cacti custom transactions summary block monitoring.
    */
-  test("Monitoring with type CactusTransactions returns transactions summary", async () => {
+  test("Monitoring with type CactiTransactions returns transactions summary", async () => {
     const monitorPromise = testWatchBlock(
-      "CactusTransactionsTest",
-      WatchBlocksListenerTypeV1.CactusTransactions,
+      "CactiTransactionsTest",
+      WatchBlocksListenerTypeV1.CactiTransactions,
       (event) => {
         expect(event).toBeTruthy();
 
-        if (!("cactusTransactionsEvents" in event)) {
+        if (!("cactiTransactionsEvents" in event)) {
           throw new Error(
             `Unexpected response from the connector: ${JSON.stringify(event)}`,
           );
         }
 
-        const eventData = event.cactusTransactionsEvents;
+        const eventData = event.cactiTransactionsEvents;
         expect(eventData.length).toBeGreaterThan(0);
         expect(eventData[0].chaincodeId).toBeTruthy();
         expect(eventData[0].transactionId).toBeTruthy();
@@ -413,9 +413,66 @@ describe("watchBlocksDelegatedSignV1 of fabric connector tests", () => {
     await monitorPromise;
   });
 
+  /**
+   * Check Cacti custom full block summary block monitoring.
+   */
+  test("Monitoring with type CactiFullBlock returns block summary", async () => {
+    const monitorPromise = testWatchBlock(
+      "CactiFullBlockTest",
+      WatchBlocksListenerTypeV1.CactiFullBlock,
+      (event) => {
+        expect(event).toBeTruthy();
+
+        if (!("cactiFullEvents" in event)) {
+          throw new Error(
+            `Unexpected response from the connector: ${JSON.stringify(event)}`,
+          );
+        }
+
+        const cactiFullBlock = event.cactiFullEvents;
+
+        // Check block fields
+        expect(cactiFullBlock).toBeTruthy();
+        expect(cactiFullBlock.blockNumber).toBeDefined();
+        expect(cactiFullBlock.blockHash).toBeTruthy();
+        expect(cactiFullBlock.previousBlockHash).toBeTruthy();
+        expect(cactiFullBlock.transactionCount).toBeDefined();
+
+        // Check transaction fields
+        for (const tx of cactiFullBlock.cactiTransactionsEvents) {
+          expect(tx.hash).toBeTruthy();
+          expect(tx.channelId).toBeTruthy();
+          expect(tx.timestamp).toBeTruthy();
+          expect(tx.type).toBeTruthy();
+          expect(tx.protocolVersion).not.toBeUndefined();
+          expect(tx.epoch).not.toBeUndefined();
+
+          // Check transaction actions fields
+          for (const action of tx.actions) {
+            expect(action.functionName).toBeTruthy();
+            expect(action.functionArgs).toBeTruthy();
+            expect(action.functionArgs.length).toEqual(5);
+            expect(action.chaincodeId).toBeTruthy();
+            expect(action.creator.mspid).toBeTruthy();
+            expect(action.creator.cert).toBeTruthy();
+
+            // Check transaction action endorsement fields
+            for (const endorsement of action.endorsements) {
+              expect(endorsement.signature).toBeTruthy();
+              expect(endorsement.signer.mspid).toBeTruthy();
+              expect(endorsement.signer.cert).toBeTruthy();
+            }
+          }
+        }
+      },
+    );
+
+    await monitorPromise;
+  });
+
   test("Invalid WatchBlocksListenerTypeV1 value gets knocked down", async () => {
     const monitorPromise = testWatchBlock(
-      "CactusTransactionsTest",
+      "InvalidTypeTest",
       "Some_INVALID_WatchBlocksListenerTypeV1" as WatchBlocksListenerTypeV1,
       () => undefined, // will never reach this because it is meant to error out
       false,

packages/cactus-plugin-ledger-connector-fabric/src/test/typescript/integration/fabric-v2-2-x/fabric-watch-blocks-v1-endpoint.test.ts
@@ -60,8 +60,8 @@ const ledgerChannelName = "mychannel";
 const ledgerContractName = "basic";
 
 // Log settings
-const testLogLevel: LogLevelDesc = "TRACE"; // default: info
-const sutLogLevel: LogLevelDesc = "TRACE"; // default: info
+const testLogLevel: LogLevelDesc = "info"; // default: info
+const sutLogLevel: LogLevelDesc = "info"; // default: info
 
 // Logger setup
 const log: Logger = LoggerProvider.getOrCreate({
@@ -384,22 +384,22 @@ describe("watchBlocksV1 of fabric connector tests", () => {
   });
 
   /**
-   * Check Cactus custom transactions summary block monitoring.
+   * Check Cacti custom transactions summary block monitoring.
    */
-  test("Monitoring with type CactusTransactions returns transactions summary", async () => {
+  test("Monitoring with type CactiTransactions returns transactions summary", async () => {
     const monitorPromise = testWatchBlock(
-      "CactusTransactionsTest",
-      WatchBlocksListenerTypeV1.CactusTransactions,
+      "CactiTransactionsTest",
+      WatchBlocksListenerTypeV1.CactiTransactions,
       (event) => {
         expect(event).toBeTruthy();
 
-        if (!("cactusTransactionsEvents" in event)) {
+        if (!("cactiTransactionsEvents" in event)) {
           throw new Error(
             `Unexpected response from the connector: ${JSON.stringify(event)}`,
           );
         }
 
-        const eventData = event.cactusTransactionsEvents;
+        const eventData = event.cactiTransactionsEvents;
         expect(eventData.length).toBeGreaterThan(0);
         expect(eventData[0].chaincodeId).toBeTruthy();
         expect(eventData[0].transactionId).toBeTruthy();
@@ -411,9 +411,66 @@ describe("watchBlocksV1 of fabric connector tests", () => {
     await monitorPromise;
   });
 
+  /**
+   * Check Cacti custom full block summary block monitoring.
+   */
+  test("Monitoring with type CactiFullBlock returns block summary", async () => {
+    const monitorPromise = testWatchBlock(
+      "CactiFullBlockTest",
+      WatchBlocksListenerTypeV1.CactiFullBlock,
+      (event) => {
+        expect(event).toBeTruthy();
+
+        if (!("cactiFullEvents" in event)) {
+          throw new Error(
+            `Unexpected response from the connector: ${JSON.stringify(event)}`,
+          );
+        }
+
+        const cactiFullBlock = event.cactiFullEvents;
+
+        // Check block fields
+        expect(cactiFullBlock).toBeTruthy();
+        expect(cactiFullBlock.blockNumber).toBeDefined();
+        expect(cactiFullBlock.blockHash).toBeTruthy();
+        expect(cactiFullBlock.previousBlockHash).toBeTruthy();
+        expect(cactiFullBlock.transactionCount).toBeDefined();
+
+        // Check transaction fields
+        for (const tx of cactiFullBlock.cactiTransactionsEvents) {
+          expect(tx.hash).toBeTruthy();
+          expect(tx.channelId).toBeTruthy();
+          expect(tx.timestamp).toBeTruthy();
+          expect(tx.type).toBeTruthy();
+          expect(tx.protocolVersion).not.toBeUndefined();
+          expect(tx.epoch).not.toBeUndefined();
+
+          // Check transaction actions fields
+          for (const action of tx.actions) {
+            expect(action.functionName).toBeTruthy();
+            expect(action.functionArgs).toBeTruthy();
+            expect(action.functionArgs.length).toEqual(5);
+            expect(action.chaincodeId).toBeTruthy();
+            expect(action.creator.mspid).toBeTruthy();
+            expect(action.creator.cert).toBeTruthy();
+
+            // Check transaction action endorsement fields
+            for (const endorsement of action.endorsements) {
+              expect(endorsement.signature).toBeTruthy();
+              expect(endorsement.signer.mspid).toBeTruthy();
+              expect(endorsement.signer.cert).toBeTruthy();
+            }
+          }
+        }
+      },
+    );
+
+    await monitorPromise;
+  });
+
   test("Invalid WatchBlocksListenerTypeV1 value gets knocked down", async () => {
     const monitorPromise = testWatchBlock(
-      "CactusTransactionsTest",
+      "CactiInvalidTest",
       "Some_INVALID_WatchBlocksListenerTypeV1" as WatchBlocksListenerTypeV1,
       () => undefined, // will never reach this because it is meant to error out
       false,

packages/cactus-plugin-persistence-fabric/src/main/typescript/plugin-persistence-fabric.ts
@@ -23,6 +23,7 @@ import {
   FabricApiClient,
   GetBlockResponseV1,
   RunTransactionResponseType,
+  GetBlockResponseTypeV1,
 } from "@hyperledger/cactus-plugin-ledger-connector-fabric";
 
 import PostgresDatabaseClient from "./db-client/db-client";
@@ -416,7 +417,7 @@ export class PluginPersistenceFabric
         query: {
           blockNumber,
         },
-        skipDecode: false,
+        type: GetBlockResponseTypeV1.Full,
       });
 
     const tempBlockParse = block.data;