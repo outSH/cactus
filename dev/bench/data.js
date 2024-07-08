d003ae790900489be7c5b22c12ef1d183bec70a9
github-action-benchmark <github@users.noreply.github.com>
2024-07-08 09:23:30 +0000

    add Benchmark (benchmarkjs) benchmark result for 02efca018a962bbbf480d2d7d98e29bc3879c195

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720179033357,
+  "lastUpdate": 1720430610064,
   "repoUrl": "https://github.com/outSH/cactus",
   "entries": {
     "Benchmark": [
@@ -276,6 +276,44 @@ window.BENCHMARK_DATA = {
             "range": "±1.34%",
             "unit": "ops/sec",
             "extra": "180 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "peter.somogyvari@accenture.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "02efca018a962bbbf480d2d7d98e29bc3879c195",
+          "message": "build(docs/examples): add yarn patch to @ionic deps of example frontend\n\n1. The `main` field of the package.json of these two packages were invalid\nbecause the `main` field was set to `bundle.js` but that file is actually\nunder `ngx/bundle.js` within the package directory and therefore the value\nof it originally was invalid that caused warnings in our tooling.\n2. By using the yarn **patch** feature we overcame this problem by modifying\nthe sources of the dependencies in question: `@ionic-native/splash-screen` and\n`@ionic-native/status-bar`\n\nSigned-off-by: Peter Somogyvari <peter.somogyvari@accenture.com>",
+          "timestamp": "2024-07-05T19:29:12-07:00",
+          "tree_id": "77847aca1aabd476926837cdbabc3436914f3835",
+          "url": "https://github.com/outSH/cactus/commit/02efca018a962bbbf480d2d7d98e29bc3879c195"
+        },
+        "date": 1720430607539,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 588,
+            "range": "±1.65%",
+            "unit": "ops/sec",
+            "extra": "177 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 357,
+            "range": "±1.22%",
+            "unit": "ops/sec",
+            "extra": "181 samples"
           }
         ]
       }
